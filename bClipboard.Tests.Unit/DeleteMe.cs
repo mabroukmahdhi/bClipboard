0ccf915eead52a8aebf4a86f7b5b54c85ac7de6f
Mabrouk.Mahdhi <Mabrouk.Mahdhi@ebizcon.de>
2024-07-25 22:57:53 +0200

    INFRA: Initialize Test Project

---
+++ bClipboard.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,12 @@
+// ---------------------------------------------------------------
+// Copyright (c) All Sweet Hearted Engineers.
+// ---------------------------------------------------------------
+
+namespace bClipboard.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
